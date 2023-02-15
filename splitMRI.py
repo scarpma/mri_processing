efc3dbd1edd94955204cbb9e2fd004112a91eb8e
scarpma <scarpma@gmail.com>
2023-02-15 20:07:34 +0100

    check for tag

--- splitMRI.py
+++ splitMRI.py
@@ -61,44 +61,50 @@ if __name__ == "__main__":
     # cardiac phase (0019,10d7)
     possible_tags = [(0x2001,0x1008), (0x0019,0x10d7)]
     
-    success = 0
-    idx = 0
-    while success == 0 :
-        tag = possible_tags[idx]
-        idx += 1
 
-        for kk, directory in enumerate(directories):
-        
-          ch_name = ch_names[kk]
-          filenames = glob.glob(directory+"*.dcm")
-          assert len(filenames) > 1
-          
-          dcms = []
-          try:
-            for i in tqdm.tqdm(range(len(filenames))):
-              dcms.append(pydicom.dcmread(filenames[i], specific_tags=[tag]))
-          except:
+    filenames = glob.glob(directories[0]+"*.dcm")
+    assert len(filenames) > 1
+    dcm = pydicom.dcmread(filenames[0])
+    found = False
+    for tag_ in possible_tags:
+        if tag_ in dcm:
+            tag = tag_
+            found = True
+            del dcm
+            del filenames
             break
-        
-          pos = [int(dcm[tag].value) for dcm in dcms]
-          nPhases = max(pos) - min(pos) + 1
-        
-          print(f"ch_name: {ch_name}, nPhases: {nPhases}, nSlices: {len(dcms[1])}")
-          dcms_ = {}
-        
-          for i in range(len(dcms)):
-            if pos[i] in dcms_:
-              dcms_[pos[i]].append(filenames[i])
-            else:
-              dcms_[pos[i]] = [filenames[i]]
-        
-          for k, v in tqdm.tqdm(dcms_.items()):
-            print()
-            output_dir = osp.join(odir, str(k).zfill(3), ch_name)
-            os.makedirs(output_dir, exist_ok=True)
-            for i in range(len(v)):
-              #print(f"saving {v[i]}")
-              shutil.copyfile(v[i], osp.join(output_dir, osp.basename(v[i])))
 
-          success = 1
+    if not found:
+        sys.exit(1)
+
+    for kk, directory in enumerate(directories):
+    
+      ch_name = ch_names[kk]
+      filenames = glob.glob(directory+"*.dcm")
+      assert len(filenames) > 1
+      
+      dcms = []
+      for i in tqdm.tqdm(range(len(filenames))):
+        dcms.append(pydicom.dcmread(filenames[i], specific_tags=[tag]))
+    
+      pos = [int(dcm[tag].value) for dcm in dcms]
+      nPhases = max(pos) - min(pos) + 1
+    
+      print(f"ch_name: {ch_name}, nPhases: {nPhases}, nSlices: {len(dcms[1])}")
+      dcms_ = {}
+    
+      for i in range(len(dcms)):
+        if pos[i] in dcms_:
+          dcms_[pos[i]].append(filenames[i])
+        else:
+          dcms_[pos[i]] = [filenames[i]]
+    
+      for k, v in tqdm.tqdm(dcms_.items()):
+        print()
+        output_dir = osp.join(odir, str(k).zfill(3), ch_name)
+        os.makedirs(output_dir, exist_ok=True)
+        for i in range(len(v)):
+          #print(f"saving {v[i]}")
+          shutil.copyfile(v[i], osp.join(output_dir, osp.basename(v[i])))
+
 
